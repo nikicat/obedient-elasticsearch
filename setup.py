2509b66891004b2fdfefd4a6766ce2f6a7ff9051
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-07-30 14:33:20 +0400

    setup.py: fixed url (renamed repository)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     setuptools.setup(
         name='obedient.elasticsearch',
         version='1.3',
-        url='https://github.com/yandex-sysmon/obedient-elasticsearch',
+        url='https://github.com/yandex-sysmon/obedient.elasticsearch',
         license='GPLv3',
         author='Nikolay Bryskin',
         author_email='devel.niks@gmail.com',
