59b76cfdc71ada404b996c2bf8be20175b38ded1
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-09-19 12:27:03 +0400

    Bump version: 1.4.0 → 1.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='obedient.elasticsearch',
-        version='1.4.0',
+        version='1.5.0',
         url='https://github.com/yandex-sysmon/obedient.elasticsearch',
         license='LGPLv3',
         author='Nikolay Bryskin',
